4f8481f657beeeeae00565d814a6fafe36950c09
Sanjeet Nayak <sanjeetnayak@localhost>
2021-01-31 17:38:46 +0530

    New Sorting Algo

--- src/BubbleSort1.java
+++ src/BubbleSort1.java
@@ -3,7 +3,10 @@ public class BubbleSort1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int i = 10;
+		System.out.println(i);
+		System.out.println("Hello");
+		
 	}
 
 }
